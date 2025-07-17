8c4ea33a2dd7aaf3aab75afca560b186efee8d89
WenTee <superjimalex@gmail.com>
2025-07-17 20:54:11 +0800

    [update] modify user_test.go

--- app/repo/user/user_test.go
+++ app/repo/user/user_test.go
@@ -2,9 +2,12 @@ package user_test
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"testing"
 	UserModel "wentee/blog/app/model/mongodb/user"
 	UserRepo "wentee/blog/app/repo/user"
+	"wentee/blog/app/schema/basemodel"
 	"wentee/blog/app/testutils"
 
 	"github.com/stretchr/testify/assert"
@@ -12,6 +15,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func TestCountUser(t *testing.T) {
@@ -42,27 +46,55 @@ func TestCreateUser(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-// func TestQueryUsers(t *testing.T) {
-// 	userCol := new(testutils.MockCollection)
+func TestQueryUsers(t *testing.T) {
 
-// 	tests := []struct {
-// 		ctx   context.Context
-// 		query *basemodel.BaseQuery
-// 	}{
-// 		{
-// 			ctx: context.TODO(),
-// 			query: &basemodel.BaseQuery{
-// 				Skip:  0,
-// 				Limit: 10,
-// 			},
-// 		},
-// 		{
-// 			ctx: context.TODO(),
-// 			query: &basemodel.BaseQuery{
-// 				Skip:  0,
-// 				Limit: 0,
-// 			},
-// 		},
-// 	}
-// 	userCol.On("Find", )
-// }
+	tests := []struct {
+		ctx   context.Context
+		query *basemodel.BaseQuery
+	}{
+		{
+			ctx: context.TODO(),
+			query: &basemodel.BaseQuery{
+				Skip:  0,
+				Limit: 10,
+			},
+		},
+		{
+			ctx: context.TODO(),
+			query: &basemodel.BaseQuery{
+				Skip:  0,
+				Limit: 0,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("Skip = %d, Limit = %d", tt.query.Skip, tt.query.Limit), func(t *testing.T) {
+			userCol := new(testutils.MockCollection)
+
+			findOpts := options.Find()
+			findOpts.SetSkip(tt.query.Skip)
+			findOpts.SetLimit(tt.query.Limit)
+
+			if tt.query.Limit > 0 {
+				userCol.On("Find", tt.ctx, mock.Anything, findOpts).
+					Return(&mongo.Cursor{}, nil) // 假設用假的 cursor
+			} else {
+				userCol.On("Find", tt.ctx, mock.Anything, findOpts).
+					Return((*mongo.Cursor)(nil), errors.New("Litmit must be positive"))
+			}
+
+			repo := &UserRepo.UserRepo{
+				UserCollection: userCol,
+			}
+			_, err := repo.UserCollection.Find(tt.ctx, bson.M{}, findOpts)
+
+			if tt.query.Limit > 0 {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+
+		})
+	}
+}
